d7c3fe7379b59bdd5ebab66050ea956df0bdfa98
Vineet4265 <vineetwork89@gmail.com>
2026-02-18 16:53:30 +0530

    Day04: PrimeRange

--- day04/NumberPrime.java
+++ day04/NumberPrime.java
@@ -2,14 +2,16 @@ package day04;
 
 public class NumberPrime {
     public static boolean isPrime(int n){
-        boolean isPrime = true;
+        // boolean isPrime = true;
+        if(n == 2){
+            return true;
+        }
         for(int i = 2; i<=n-1; i++){
             if(n % i == 0){
-                isPrime = false;
-                break;
+                return false;
             }
         }
-        return isPrime;
+        return true;
     }
     public static void main (String args[]){
         System.out.println(isPrime(4));
