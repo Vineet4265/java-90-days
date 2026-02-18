713cf8c002373a2b308dd0582055c92343cf82ce
Vineet4265 <vineetwork89@gmail.com>
2026-02-18 09:35:18 +0530

    Day02: Relational operator

---
+++ day02/RelationalOperator.java
@@ -0,0 +1,10 @@
+package day02;
+
+public class RelationalOperator {
+    public static void main(String args[]){
+        int a = 10;
+        int b = 5;
+        System.out.println((a == b));
+    }
+    
+}
