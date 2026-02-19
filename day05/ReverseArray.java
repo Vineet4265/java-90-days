224ddf57bf87608845792bfd658bb51f0a5fdce4
Vineet4265 <vineetwork89@gmail.com>
2026-02-19 10:29:44 +0530

    Day05: Reverse array

---
+++ day05/ReverseArray.java
@@ -0,0 +1,23 @@
+package day05;
+
+public class ReverseArray {
+    public static void reverse(int numbers[]){
+        int start = 0; int end = numbers.length-1;
+        while(start<end){
+        //swap
+        int temp = numbers[start];
+        numbers[start] = numbers[end];
+        numbers[end] = temp;
+        start++;
+        end--;
+        }
+    }
+    public static void main(String args[]){
+        int numbers[] = {2, 4, 6, 8, 10};
+        reverse(numbers);
+        for(int i = 0; i<numbers.length; i++){
+            System.out.print(numbers[i]+" ");
+        }
+        System.out.println();
+    }   
+}
